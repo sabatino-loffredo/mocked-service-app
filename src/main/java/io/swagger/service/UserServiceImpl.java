26f52bf31a37e21b72024683bb4f15532a256429
Salvatore Di Palo <salvatore.dipalo@stepsrl.org>
2020-07-08 00:57:29 +0200

    Temp fix

--- src/main/java/io/swagger/service/UserServiceImpl.java
+++ src/main/java/io/swagger/service/UserServiceImpl.java
@@ -23,7 +23,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public SearchUserInfoResponse getUsers(String codiceFiscale, Integer page, Integer size) throws IOException, ParseException {
-        SearchUserInfoResponse searchUserInfoResponse = JsonUtil.readMockData();
+        //SearchUserInfoResponse searchUserInfoResponse = JsonUtil.readMockData();
+
+        SearchUserInfoResponse searchUserInfoResponse = objectMapper.readValue(MockedData.usersMockedData, SearchUserInfoResponse.class);
 
         if(codiceFiscale != null){
             for (Iterator<SearchUserInfoGetObject> iterator = searchUserInfoResponse.getResults().iterator(); iterator.hasNext();) {
